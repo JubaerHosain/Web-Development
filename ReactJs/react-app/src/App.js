36fd22cd1dac8d33d383767d6467431523ef3c4d
JubaerHosain <jubaerhosain1119@gmail.com>
2022-12-13 20:54:49 +0600

    Updated

--- ReactJs/react-app/src/App.js
+++ ReactJs/react-app/src/App.js
@@ -1,31 +1,13 @@
-import React from "react";
-import Section from "./Context API/Section";
-import ThemeContext from "./Context API/ThemeContext";
+// import Form from "./Hooks/UseAndForwardRef/Form";
+import Time from "./Hooks/UseAndForwardRef/Timer";
 
-export default class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            theme: "black",
-            changeBackground: this.changeBackground,
-        };
-    }
-
-    changeBackground = () => {
-        const theme = this.state.theme === "black" ? "green" : "black";
-        this.setState({ theme: theme });
-    };
-
-    render() {
-        console.log("App Rendered");
-        // value hisebe state patano uchit
-        // otherwise value change na hoilew prottek bar notun ekta object reference jabe
-        return (
-            <div className="app">
-                <ThemeContext.Provider value={this.state}>
-                    <Section />
-                </ThemeContext.Provider>
-            </div>
-        );
-    }
+function App() {
+    return (
+        <div className="app">
+            {/** <Form />*/}
+            <Time />
+        </div>
+    );
 }
+
+export default App;
