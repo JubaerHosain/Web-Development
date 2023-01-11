f9978c7a614959068c1ff3ee4e013ed7d4931cb6
JubaerHosain <jubaerhosain1119@gmail.com>
2023-01-11 11:59:48 +0600

    Updated

---
+++ NodeJs/Cluster/multiple_instance.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+const cluster = require("cluster");
+const os = require("os");
+
+const noOfCPU = os.cpus().length;
+
+app.get("/", (req, res) => {
+    console.log("GET");
+    // for (let i = 0; i < 1e1; i++) {
+    //     // console
+    // }
+    res.send(`Worker ${process.pid} sends Hello World`);
+
+    // kill the worker
+    cluster.worker.kill();
+});
+
+// all workers share the same port number
+if (cluster.isMaster) {
+    for (let i = 0; i < noOfCPU; i++) {
+        cluster.fork();
+    }
+
+    // listen even
+    cluster.on("exit", (worker, code, signal) => {
+        console.log(`Worker ${worker.process.pid} exited with code ${code}`);
+        // create a new worker after killing current worker such that
+        // our total alive workers remains same
+        cluster.fork();
+    });
+} else {
+    app.listen(3000, () => {
+        console.log(`Worker ${process.pid} is listening on port 3000`);
+    });
+}
